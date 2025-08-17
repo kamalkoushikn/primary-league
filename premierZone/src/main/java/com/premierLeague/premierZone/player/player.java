f37c076369abdbefbfc0d242786f9d326d28fe91
kamalkoushikn <>
2025-08-16 23:25:19 -0700

    init

---
+++ premierZone/src/main/java/com/premierLeague/premierZone/player/player.java
@@ -0,0 +1,28 @@
+package com.premierLeague.premierZone.player;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="player_stats")
+public class player {
+    @Id
+    @Column(name = "name",unique = true)
+    private String name;
+    private String nation;
+    private String pos;
+    private Integer age;
+    private Integer mp;
+    private Integer starts;
+    private Double min;
+    private Double gls;
+    private Double ast;
+    private Double pk;
+    private Double crdy;
+    private Double crdr ;
+    private Double xg;
+    private Double xag;
+    private String team;
+}
---
+++ premierZone/src/test/java/com/premierLeague/premierZone/PremierZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premierLeague.premierZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
